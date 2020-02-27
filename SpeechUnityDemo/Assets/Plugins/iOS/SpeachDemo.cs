c563133bfa2d5d1120fbf7cd554579b4b7cff5c3
Bogdan Veliscu <bogdan.veliscu@outlook.com>
2020-02-27 15:10:17 +0200

    added the needed unity bindings and a demo for the speech recognition

---
+++ SpeechUnityDemo/Assets/Plugins/iOS/SpeachDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeachDemo : MonoBehaviour
+{
+	public Text textField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #if UNITY_IPHONE
+            SpeechNativePlugin._init();
+
+            SpeechNativePlugin._listenWithCallback(OnTextRecognized);
+		#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+
+        #if UNITY_IPHONE
+			SpeechNativePlugin._release();
+		#endif
+    }
+
+    void OnTextRecognized(string message){
+    	Debug.LogFormat("### OnTextRecognized: [{0}]  ");
+    	textField.text = message;
+    }
+}
---
+++ SpeechUnityDemo/Assets/Plugins/iOS/SpeechNativePlugin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using AOT;
+using UnityEngine;
+
+public static partial class SpeechNativePlugin{
+
+	#if UNITY_IPHONE
+    [DllImport("__Internal")]
+    public static extern void _init();
+
+    [DllImport("__Internal")]
+    private static extern void _listen(OnRecognized callback);
+
+    [DllImport("__Internal")]
+    public static extern void _release();
+
+    [DllImport("__Internal")]
+    public static extern void _getAudioSamples(float[] samples, out int size);
+
+
+    public delegate void OnRecognized(string mesage);
+
+    [MonoPInvokeCallback(typeof(OnRecognized))]
+    public static void OnSpeachRecognized(string message){
+        Debug.Log("# SpeachRecognitionNative : " + message);
+
+        if(OnSpeachRecognizeCompleted != null){
+            OnSpeachRecognizeCompleted(message);
+        }
+    }
+
+    public static Action<string> OnSpeachRecognizeCompleted;
+
+    public static void _listenWithCallback(Action<string> onRecognized){
+        OnSpeachRecognizeCompleted = onRecognized;
+
+        _listen(OnSpeachRecognized);
+    }
+
+#endif
+
+}
